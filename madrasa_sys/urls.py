d3407a3ec854d3a7d8429159ad84078c97f9eec0
PM <masoud180mohd@gmail.com>
2026-07-08 12:39:28 +0300

    API: Nimeongeza njia ya mzizi mkuu ili kuzuia 404 ya simu

--- madrasa_sys/urls.py
+++ madrasa_sys/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Tumeongeza hii
-from django.conf.urls.static import static # Tumeongeza hii
+from django.conf import settings 
+from django.conf.urls.static import static 
+from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse 
+
+# Hii ni kazi ndogo itakayojibu programu ya simu kuwa server ipo hai
+def salamu_ya_api(request):
+    return HttpResponse("Muunganisho umefanikiwa!", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('madrasa/', include('usimamizi.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('', salamu_ya_api, name='home_ping'),
 ]
 
-# Tumeongeza mstari huu chini kuruhusu picha kuonekana
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
